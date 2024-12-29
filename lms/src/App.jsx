1978fb32e3497c606fb2e1a407cfdaa0b0a92af4
Ashu-tosh1 <ashutos1201@gmail.com>
2024-12-29 22:32:33 +0530

    commit

---
+++ lms/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Button } from './components/ui/button'
+import Login from './Pages/Login'
+
+function App() {
+
+
+  return (
+    <div>
+<Login/>
+
+    </div>
+  )
+}
+
+export default App
